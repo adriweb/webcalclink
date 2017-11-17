2d2100b7831fdb155f10fd89685188bfa3dcb3f0
Adrien Bertrand <bertrand.adrien@gmail.com>
2017-11-17 02:01:00 +0100

    Simplify transferIn/Out methods and rewrite readyRequest

--- webusbti.js
+++ webusbti.js
@@ -93,75 +93,49 @@ function chunk_buffer(data)
         return this.device_.close();
     };
 
-    webusb.Device.prototype.controlTransferOut = function (setup, data)
+    webusb.Device.prototype.transferOut = function (data)
     {
-        return this.device_.controlTransferOut(setup, data);
-    };
-
-    webusb.Device.prototype.controlTransferIn = function (setup, length)
-    {
-        return this.device_.controlTransferIn(setup, length);
-    };
-
-    webusb.Device.prototype.transferOut = function (ep, data)
-    {
-        console.debug(`transferOut\t| ep = ${ep}\t| data = ${data}`);
+        console.debug(`transferOut\t| data = ${data}`);
         let str = ""+buf2hex(data);
         str = str.substr(0, 8) + thinsp + str.substr(8, 2) + thinsp + str.substr(10);
         this.gui.log[0].innerHTML += `<span class='out'>${moment().format('HH:mm:ss.SSS')} &gt; ${str}</span></br>`;
         /************************/
-        return this.device_.transferOut(ep, data);
+        return this.device_.transferOut(2, data);
     };
 
-    webusb.Device.prototype.transferIn = function (ep, length)
+    webusb.Device.prototype.transferInWithLen = function (length)
     {
-        console.debug(`transferIn\t| ep = ${ep}\t| length = ${length}`);
-        /************************/
-        return this.device_.transferIn(ep, length);
+        console.debug(`transferInWithLen\t| length = ${length}`);
+        return this.device_.transferIn(1, length);
+    };
+
+    webusb.Device.prototype.transferIn = function ()
+    {
+        return this.transferInWithLen(64);
+    };
+
+    webusb.Device.prototype.responseHandler = function(result)
+    {
+        let str = ""+buf2hex(result.data.buffer);
+        str = str.substr(0, 8) + thinsp + str.substr(8, 2) + thinsp + str.substr(10);
+        this.gui.log[0].innerHTML += `<span class='in'>${moment().format('HH:mm:ss.SSS')} &lt; ${str}</span></br>`;
     };
 
     webusb.Device.prototype.readySequence = function ()
     {
-        const dev = this;
-
-        // array of: [ hexStr, [ number of response bytes... ] ]
-        const seq = [
-            [ "00000004 01 00000400",                         [ 9 ]     ], // response  0000000402000003ff (9)
-            [ "00000010 04 0000000a0001000300010000000007D0", [ 7, 15 ] ], // responses 0000000205e000 (7) and 0000000a04000000040012000007D0 (15)
-            [ "00000002 05 e000",                             [ ]       ]
-        ];
-
-        async function responseFunc(result)
-        {
-            // console.log("received: ", result);
-            const dataBufArray = buf2hex(result.data.buffer);
-            console.log("dataBufArray received: ", dataBufArray);
-            let str = ""+dataBufArray;
-            str = str.substr(0, 8) + thinsp + str.substr(8, 2) + thinsp + str.substr(10);
-            dev.gui.log[0].innerHTML += `<span class='in'>${moment().format('HH:mm:ss.SSS')} &lt; ${str}</span></br>`;
-        }
-
-        // todo : compute the length from the first 4 (5 later, actually...) bytes
-
         this.connect().then(async () =>
         {
-            for (let i = 0; i < seq.length; i++)
+            // array of: [ hexStr, readCountAfter ]
+            const seq = [
+                [ "00000004 01 00000400",                         1 ], // response  0000000402000003ff (9)
+                [ "00000010 04 0000000a0001000300010000000007D0", 2 ], // responses 0000000205e000 (7) and 0000000a04000000040012000007D0 (15)
+                [ "00000002 05 e000",                             0 ]
+            ];
+            for (const frame of seq)
             {
-                const seqPair = seq[i];
-                switch (seqPair[1].length)
-                {
-                    case 0:
-                        await this.transferOut(2, hexStrToArrayBuffer(seqPair[0]));
-                        break;
-                    case 1:
-                        await this.transferOut(2, hexStrToArrayBuffer(seqPair[0]))
-                            .then(() => this.transferIn(1, seqPair[1][0])).then(responseFunc);
-                        break;
-                    case 2:
-                        await this.transferOut(2, hexStrToArrayBuffer(seqPair[0]))
-                            .then(() => this.transferIn(1, seqPair[1][0])).then(responseFunc)
-                            .then(() => this.transferIn(1, seqPair[1][1])).then(responseFunc);
-                        break;
+                await this.transferOut(hexStrToArrayBuffer(frame[0]));
+                for (let i=0; i<frame[1]; i++) {
+                    await this.transferIn().then(this.responseHandler.bind(this));
                 }
             }
         })
